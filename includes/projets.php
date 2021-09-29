d42b222197201f48f7698b83c1cc522ee413860a
WilliamsChazal <w.chazal@codeur.online>
2021-09-29 16:56:36 +0200

    changement curseur & bottomnav

--- includes/projets.php
+++ includes/projets.php
@@ -1,6 +1,7 @@
 <div class="list_projet">
-    <div class='modal-content'> 
-        <div id="mydivheader"></div>
+
+    <div class='modal-content' > 
+
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod soluta ratione pariatur doloribus accusantium culpa necessitatibus quam eligendi voluptate, corrupti doloremque, incidunt, dolorum tenetur fuga neque. Suscipit eligendi sit reiciendis.</p></div>
    
 </div>
