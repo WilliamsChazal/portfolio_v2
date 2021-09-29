04771d049762ec71d4e9727658a5e45c5aa3fcc2
WilliamsChazal <w.chazal@codeur.online>
2021-09-29 17:15:19 +0200

    W modal projet

--- includes/projets.php
+++ includes/projets.php
@@ -1,7 +1,11 @@
 <div class="list_projet">
 
     <div class='modal-content' > 
-
+        <div>
+            <span><h3>Williams.Chazal ><strong>Porfolio</strong></span> </h3>
+            <span>afficher par :</span>
+            <span class='close'></span>
+    </div> 
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod soluta ratione pariatur doloribus accusantium culpa necessitatibus quam eligendi voluptate, corrupti doloremque, incidunt, dolorum tenetur fuga neque. Suscipit eligendi sit reiciendis.</p></div>
    
 </div>
