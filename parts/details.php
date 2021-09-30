fb70b5a92948ac6d4ea1b59bdce141a83d2176b1
WilliamsChazal <w.chazal@codeur.online>
2021-09-30 17:59:51 +0200

    modal projet-details dispsition Ok

--- parts/details.php
+++ parts/details.php
@@ -6,7 +6,7 @@
             <span><img src="../assets/icons/logo-blindness.svg" alt="" srcset=""></span>
         </div>
         <div class='details-projets'>
-            <span><img src="../assets/img/photo-1.png" alt="" srcset=""></span>
+            <span><img src="../assets/img/capture-blindness.png" alt="" srcset=""></span>
             <span>
                 <h3>blindness</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio doloremque minus dignissimos veniam eius atque, cupiditate repudiandae tempore delectus exercitationem, blanditiis quos accusamus aspernatur cumque consectetur vero dolor asperiores quam!</p>
