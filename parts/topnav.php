b813011e1bad548cbdf420c4abb3a83486ec9229
WilliamsChazal <w.chazal@codeur.online>
2021-10-12 22:23:17 +0200

    modif log topnav

--- parts/topnav.php
+++ parts/topnav.php
@@ -1,6 +1,6 @@
 <nav class="topnav">
     <div>
-        <span id='logo'><img src="../assets/img/logo.png" alt="" srcset=""></span>
+        <span id='logo'><!-- <img src="../assets/img/logo.png" alt="" srcset=""> --></span>
         <button calss ='close' id='btn'><h2>Williams Chazal</h2></button>
         
     </div>
