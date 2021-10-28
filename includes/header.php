e963016ca86c3220771a0f8cf1759fee2945d6ca
WilliamsChazal <w.chazal@codeur.online>
2021-10-28 11:24:38 +0200

    changement tailles police & debut modal skills

--- includes/header.php
+++ includes/header.php
@@ -5,12 +5,14 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../styles/main.css">
+        <link rel="stylesheet" href="../styles/libs/ressources.css">
         <link rel="stylesheet" href="../styles/libs/topnav.css">
         <link rel="stylesheet" href="../styles/libs/switch.css">
         <link rel="stylesheet" href="../styles/libs/bottomNav.css">
         <link rel="stylesheet" href="../styles/dark-mode.css">
         <link rel="stylesheet" href="../styles/projets.css">
         <link rel="stylesheet" href="../styles/details.css">
+        <link rel="stylesheet" href="../styles/skills.css">
         <link rel="stylesheet" href="../styles/libs/task_bar.css">
         <title>Williams Chazal</title>
     </head>
