0b1d25389f6b2f2261c5ea07a198b114c504267f
WilliamsChazal <w.chazal@codeur.online>
2021-10-28 12:15:21 +0200

    ajout script modal skills

--- includes/footer.php
+++ includes/footer.php
@@ -2,5 +2,6 @@
 
 <script src="/portfolio_v2/scripts/main.js"></script>
 <script src="/portfolio_v2/scripts/topnav.js"></script>
+<script src="/portfolio_v2/scripts/skills.js"></script>
 </body>
 </html>
--- parts/skills.php
+++ parts/skills.php
@@ -1,16 +1,28 @@
 <section class='modal_skills'>
 
     <div class='modal_skills_content'>
-        <div  class='modal_skills_content--child'>
+        <div  class='modal_skills_content--child '>
             <span><h2>Que sais je faire</h2></span>
             <span><p>Conception</p></span>
             <span><p>Développement web</p></span>
             <span><p>Logiciels et technologie</p></span>
         </div>
         <div class='modal_skills_content--child--1'>
-            <span><img src="../assets/icons/conception.svg" alt="" srcset=""></span>
-            <span><h2>Conception</h2></span>
-            <span><p>Création d’identité graphique, logos, chartes graphiques, visuels et documents. Typographie, dessin vectoriel, retouche photo. Création de thèmes graphiques et d’interfaces web. Responsive design. Ergonomie. Développement front-end selon les standards W3C.</p></span>
+            <div class='slides'>
+                <span><img src="../assets/icons/conception.svg" alt="" srcset=""></span>
+                <span><h2>Conception</h2></span>
+                <span><p>Création d’identité graphique, logos, chartes graphiques, visuels et documents. Typographie, dessin vectoriel, retouche photo. Création de thèmes graphiques et d’interfaces web. Responsive design. Ergonomie. Développement front-end selon les standards W3C.</p></span>
+            </div>
+            <div class='slides'>
+                <span><img src="../assets/icons/conception.svg" alt="" srcset=""></span>
+                <span><h2>Conception</h2></span>
+                <span><p>Création d’identité graphique, logos, chartes graphiques, visuels et documents. Typographie, dessin vectoriel, retouche photo. Création de thèmes graphiques et d’interfaces web. Responsive design. Ergonomie. Développement front-end selon les standards W3C.</p></span>
+            </div>
+            <div class='slides'>
+                <span><img src="../assets/icons/conception.svg" alt="" srcset=""></span>
+                <span><h2>Conception</h2></span>
+                <span><p>Création d’identité graphique, logos, chartes graphiques, visuels et documents. Typographie, dessin vectoriel, retouche photo. Création de thèmes graphiques et d’interfaces web. Responsive design. Ergonomie. Développement front-end selon les standards W3C.</p></span>
+            </div>
         </div>
     </div>
 
