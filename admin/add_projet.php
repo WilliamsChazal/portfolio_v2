25788394cd4066e37b5f2dc0738fccadaf47b41c
WilliamsChazal <w.chazal@codeur.online>
2021-11-03 12:01:32 +0100

    pb upload image

--- admin/add_projet.php
+++ admin/add_projet.php
@@ -12,27 +12,33 @@ session_start();
 </head>
 <body>
 
-            <form action="projet_handler.php" method="post" enctype="multipart/form-data"> 
+    <form action="projet_handler.php" method="post" enctype="multipart/form-data"> 
+
         <div>
             <label for="input_titre">Titre</label>
             <input type="text" id="input_titre" name="projet_titre">
         </div>
+
         <div>
             <label for="input_debut">Date de démarrage</label>
             <input type="date" id="input_debut" name="projet_debut">
         </div>
+
         <div>
             <label for="input_end">Date de fin</label>
             <input type="date" id="input_end" name="projet_fin">
         </div>
+
         <div>
             <label for="input_context">Contexte</label>
             <textarea name="projet_context" id="input_context" cols="30" rows="10"></textarea>
         </div>
+
         <div>
             <label for="input_specs">Spécifications fonctionnelles</label>
             <textarea name="projet_specs" id="input_specs" cols="30" rows="10"></textarea>
         </div>
+
         <div>
             <label for="input_type">Type du projet</label>
             <select name="projet_type" id="input_type">
@@ -40,18 +46,22 @@ session_start();
                 <option value="web">Web</option>
             </select>
         </div>
+
         <div>
             <label for="input_image">Capture du projet</label>
             <input type="file" id="input_image" name="projet_image">
         </div>
+
         <div>
             <label for="input_logo">Logo</label>
             <input type="file" id="input_logo" name="projet_logo">
         </div>
+
         <div>
             <label for="input_githublink">Lien GitHub</label>
             <input type="text" id="input_githublink" name="projet_githublink">
         </div>
+
         <div>
             <label for="input_link">Lien du projet</label>
             <input type="text" id="input_link" name="projet_link">
