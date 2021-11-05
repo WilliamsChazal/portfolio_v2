aa5b3e4f4e031c62067112af06b57d79a4d7a4a3
WilliamsChazal <w.chazal@codeur.online>
2021-11-05 11:29:51 +0100

    skills ok

--- parts/skills.php
+++ parts/skills.php
@@ -1,26 +1,49 @@
-
+<?php
+    /* if(isset($_GET['id']) &&!empty($_GET['id'])) { */
+        require_once('../admin/db-connect.php');
+        /* $id = strip_tags($_GET['id']); */
+        /* $sql ='SELECT*FROM `projets` WHERE `idprojet`=:id'; */
+        $sql ='SELECT*FROM `skills`';
+        $query = $db->prepare($sql);
+      /*   $query->bindValue(':id', $id, PDO::PARAM_STR); */
+        $query ->execute();
+        $result = $query->fetchAll();
+        /* var_dump($result); */
+    /* }else{
+        echo'id manquante';
+    } */
+?>
     <div class='modal_skills_content '>
         <div  class='modal_skills_content--child '>
             <span><h2>Que sais je faire</h2></span>
-            <span><p class='demo cursor' onclick="currentSlide(1)">Conception</p></span>
-            <span><p class='demo cursor' onclick="currentSlide(2)">Développement web</p></span>
-            <span><p class='demo cursor' onclick="currentSlide(3)">Logiciels et technologie</p></span>
+            <?php
+
+$i=0;
+        foreach ($result as $skills) {
+    ?>
+            <span><p class='demo cursor' onclick="currentSlide(<?=$i+1?>)"><?= htmlspecialchars($skills["skills_titre"],ENT_QUOTES)?></p></span>
+            
+        <?php
+            $i++;
+                }
+      ?>
         </div>
         <div class='modal_skills_content--child--1'>
-            <div class='slides'>
-                <span><img src="../assets/icons/conception.svg" alt="" srcset=""></span>
-                <span><h2>Conception</h2></span>
-                <span><p>Création d’identité graphique, logos, chartes graphiques, visuels et documents. Typographie, dessin vectoriel, retouche photo. Création de thèmes graphiques et d’interfaces web. Responsive design. Ergonomie. Développement front-end selon les standards W3C.</p></span>
+
+
+        <?php
+
+        $i=0;
+                foreach ($result as $skills) {
+            ?>
+            <div class='slides' style=" <?= $i==0? "display:block" :"display:none" ?>">
+                <span><img src="../admin/assets_admin/admin_logo/<?=$skills['skills_logo']?>" alt="" srcset=""></span>
+                <span><h2><?= htmlspecialchars($skills["skills_titre"],ENT_QUOTES)?></h2></span>
+                <span><p><?=$skills['skills_texte']?></p></span>
             </div>
-            <div class='slides' style='display:none'>
-                <span><img src="../assets/icons/conception.svg" alt="" srcset=""></span>
-                <span><h2>Développement web</h2></span>
-                <span><p>Développement sur mesure. Création ou refonte de site. Systèmes de gestion de contenu (CMS)WordPress. Technologies open-source. HTML / CSS / JavaScript / PHP. </p></span>
-            </div>
-            <div class='slides' style='display:none'>>
-                <span><img src="../assets/icons/conception.svg" alt="" srcset=""></span>
-                <span><h2>Logiciels et technologie</h2></span>
-                <span><p>Création d’identité graphique, logos, chartes graphiques, visuels et documents. Typographie, dessin vectoriel, retouche photo. Création de thèmes graphiques et d’interfaces web. Responsive design. Ergonomie. Développement front-end selon les standards W3C.</p></span>
-            </div>
+            <?php
+            $i++;
+                }
+      ?>
         </div>
     </div>
