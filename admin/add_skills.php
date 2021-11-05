538faf79c1f44ba7dead16ea6524011f118a03fa
WilliamsChazal <w.chazal@codeur.online>
2021-11-05 09:55:43 +0100

    ajout handler skills

--- admin/add_skills.php
+++ admin/add_skills.php
@@ -18,16 +18,19 @@ session_start();
 <titre>Ajouter une compétences</titre> <br>
 <div class="mb-3">
   <label for="input_titre" class="form-label">Titre</label>
-  <input type="texte" class="form-control" id="exampleFormControlInput1" placeholder="Titre de la compétence">
+  <input type="texte" class="form-control" name="skills_titre" id="exampleFormControlInput1" placeholder="Titre de la compétence">
 </div>
 <div class="mb-3">
   <label for="input_texte" class="form-label">Description</label>
-  <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Description de la compétence" rows="3"></textarea>
+  <textarea class="form-control" name="skills_texte" id="exampleFormControlTextarea1" placeholder="Description de la compétence" rows="3"></textarea>
 </div>
 <div class="mb-3">
   <label for="input_logo" class="form-label">Logo de la compétence</label>
-  <input class="form-control" type="file" id="formFile">
+  <input class="form-control"  name="skills_logo" type="file" id="formFile">
 </div>
+<div class="col-auto">
+    <button type="submit" class="btn btn-primary mb-3">Enregistrer la compétence</button>
+  </div>
 </form>
 </div>
 
