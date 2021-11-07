4a9f8fe4352ea52b105f8ca5d6b23a3a4d512077
WilliamsChazal <w.chazal@codeur.online>
2021-11-07 15:41:06 +0100

    appel depuis bbd modal designs

--- parts/designs.php
+++ parts/designs.php
@@ -1,57 +1,44 @@
 <!-- <div><h1>Conception visuelle</h1></div> -->
-
+<?php
+    /* if(isset($_GET['id']) &&!empty($_GET['id'])) { */
+        require_once('../admin/db-connect.php');
+        /* $id = strip_tags($_GET['id']); */
+        /* $sql ='SELECT*FROM `projets` WHERE `idprojet`=:id'; */
+        $sql ='SELECT*FROM `designs`';
+        $query = $db->prepare($sql);
+      /*   $query->bindValue(':id', $id, PDO::PARAM_STR); */
+        $query ->execute();
+        $result = $query->fetchAll();
+        /* var_dump($result); */
+    /* }else{
+        echo'id manquante';
+    } */
+?>
 <div class="grid">
   <div class="grid-sizer"></div>
+
   <div class="grid-item">
-    <img src="../assets/img/maquette_yoka.png" />
-      <span>
-      <h5>Titre</h5>
-    <p>Descirption de la maquette</p>
-    <button>Voir le Design</button>
-      </span>
+  <?php
+                foreach ($result as $designs) {
+            ?>
+    <img src="../admin/assets_admin/admin_design/<?=$designs['design_thumbnail']?>"/>
+    <h5><?=$designs['design_titre']?></h5>
+    <p><?=$designs['design_texte']?></p>
+   <a href="../admin/assets_admin/admin_design/<?=$designs['design_file']?>" target="_blank" rel="noopener noreferrer"> <button>Voir la maquette : <?=$designs['design_titre']?> </button></a>
+   <?php
+                }
+      ?>
   </div>
-  <div class="grid-item">
-    <img src="../assets/img/thumbnail_victuaille.png" />
-    <h5>Titre</h5>
-    <p>Descirption de la maquette</p>
-   <a href="../assets/img/maquette_victuaille.png" target="_blank" rel="noopener noreferrer"> <button>Voir le Design</button></a>
-  </div>
-  <div class="grid-item">
-    <img src="../assets/img/maquette_yupanki.png" />
-    <h5>Titre</h5>
-    <p>Descirption de la maquette</p>
-    <button>Voir le Design</button>
-  </div>
-  <div class="grid-item">
-    <img src="../assets/img/maquette_thomas.png" />
-    <h5>Titre</h5>
-    <p>Descirption de la maquette</p>
-    <button>Voir le Design</button>
-  </div>
-  <div class="grid-item">
-   <a href="../assets/img/maquette_bourgogne-defib.png" target="_blank" rel="noopener noreferrer"><img src="../assets/img/maquette_bourgogne-defib.png" /></a>
-   <h5>Titre</h5>
-    <p>Descirption de la maquette</p>
-    <button>Voir le Design</button>
-  </div>
-  <div class="grid-item">
-    <img src="../assets/img/maquette_esprit-sud.png" />
-    <h5>Titre</h5>
-    <p>Descirption de la maquette</p>
-    <button>Voir le Design</button>
-  </div>
-  <div class="grid-item">
-    <img src="../assets/img/maquette_esprit-sud.png" />
-    <h5>Titre</h5>
-    <p>Descirption de la maquette</p>
-    <button>Voir le Design</button>
-  </div>
-  <div class="grid-item">
-    <img src="../assets/img/maquette_esprit-sud.png" />
-    <h5>Titre</h5>
-    <p>Descirption de la maquette</p>
-    <button>Voir le Design</button>
-  </div>
+
  
   </div>
 </div>
+
+
+    
+      <span>
+      
+    
+    <button>Voir le Design</button>
+      </span>
+     
