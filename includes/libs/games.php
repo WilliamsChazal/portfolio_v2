141b2c565316d6c1e106614fe692de3c9f28a926
WilliamsChazal <w.chazal@codeur.online>
2021-11-08 10:46:35 +0100

    responsive tablet ok & ajout affichage icones jeux

--- includes/libs/games.php
+++ includes/libs/games.php
@@ -1,21 +1,37 @@
+<?php
+    /* if(isset($_GET['id']) &&!empty($_GET['id'])) { */
+        require_once('./../admin/db-connect.php');
+        /* $id = strip_tags($_GET['id']); */
+        /* $sql ='SELECT*FROM `projets` WHERE `idprojet`=:id'; */
+        $sql ='SELECT * FROM  projets WHERE projet_type = "jeu"';
+        $query = $db->prepare($sql);
+      /*   $query->bindValue(':id', $id, PDO::PARAM_STR); */
+        $query ->execute();
+        $result = $query->fetchAll();
+        /* var_dump($result); */
+    /* }else{
+        echo'id manquante';
+    } */
+?>
+
+
 <section class='games'>
     <div><h4 class='text-pop-up-top' >Mes Jeux</h4></div>
         <div>
-            <span onclick="displayIframe()" class='heartbeat'> 
-                <img src="../assets/icons/ico-bomb.svg" alt="" srcset="">
+        <?php
+                foreach ($result as $projet) {
+            ?>
+            <span onclick="displayIframe('<?=$projet['projet_link']?>')" class='heartbeat'> 
+                <img src="../admin/assets_admin/admin_logo/<?=$projet['projet_logo']?>" alt="" srcset="">
             </span>
-            <span onclick="displayIframe()">
-                <p>WolfenBomberSteinMan</p>
+            <span onclick="displayIframe('<?=$projet['projet_link']?>')">
+                <p><?=$projet['projet_titre']?></p>
             </span>
-            <span  onclick ="removeIFrame()"><i class="fa fa-times-circle" aria-hidden="true" id="close_games"></i></span>
-        </div>
-        <div>
-            <span class='heartbeat'>
-            <img src="../assets/icons/ico-jeux.svg" alt="" srcset="">
-            </span>
+            <span  onclick ="removeIFrame('<?=$projet['projet_link']?>')"><i class="fa fa-times-circle" aria-hidden="true" id="close_games"></i></span>
+            <?php
+                }
+      ?>
         
-        <span><p>Titre du jeu</p></span>
-        </div>
        
 </section>
 
@@ -23,16 +39,17 @@
 <script>
 
 
-function displayIframe() {
+function displayIframe(projet_link) {
         let frame = document.getElementById("iframeDisplay");
         
-        document.getElementById("iframeDisplay").innerHTML = "<iframe src=\"https://williamsc.promo-68.codeur.online/wolfenbomberstein/\" height=\"100%\" width=\"100%\" ></iframe>";
+        document.getElementById("iframeDisplay").innerHTML = "<iframe src='"+projet_link+"' height=\"100%\" width=\"100%\" ></iframe>";
             frame.style.display ='block'
         document.getElementById('close_games').style.display ='block';
         
         }
-function removeIFrame() {
+function removeIFrame(projet_link) {
       let frame = document.getElementById("iframeDisplay");
       frame.style.display ='none';
+      document.getElementById('close_games').style.display ='none';
 }
 </script>
