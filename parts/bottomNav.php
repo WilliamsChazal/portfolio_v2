6d4f955f4191903dafb8ec5ed6836c49ac27727f
WilliamsChazal <w.chazal@codeur.online>
2021-11-08 11:16:24 +0100

    ajout lien random bottom nav

--- parts/bottomNav.php
+++ parts/bottomNav.php
@@ -1,3 +1,20 @@
+<?php
+    /* if(isset($_GET['id']) &&!empty($_GET['id'])) { */
+        require_once('../admin/db-connect.php');
+        /* $id = strip_tags($_GET['id']); */
+        /* $sql ='SELECT*FROM `projets` WHERE `idprojet`=:id'; */
+        $sql ='SELECT*FROM `projets` ORDER BY RAND() LIMIT 1';
+        $query = $db->prepare($sql);
+      /*   $query->bindValue(':id', $id, PDO::PARAM_STR); */
+        $query ->execute();
+        $result = $query->fetchAll();
+        /* var_dump($result); */
+    /* }else{
+        echo'id manquante';
+    } */
+?>
+
+
 <section class="bottomNav">
 <nav >
     <div class='hover' onclick='openModal("projets.php", "modal_projets" ,"Portfolio", 10)' >
@@ -19,10 +36,16 @@
         <p>Contact</p>
     </div>
     <div id='projet' class='hover'>
-        <a href="https://esprit-sud01.fr/" target='_blank'>
-        <img src="../assets/icons/logo_esprit_sud.png" alt="" srcset="">
-        <p>Esprit Sud</p>
+    <?php
+                foreach ($result as $projet) {
+            ?>
+        <a href="<?=$projet['projet_link']?>" target='_blank'>
+        <img src="../admin/assets_admin/admin_logo/<?=$projet['projet_logo']?>" alt="" srcset="">
+        <p><?=$projet['projet_titre']?></p>
         </a>
+        <?php
+                }
+      ?>
     </div>
     
 </nav>
