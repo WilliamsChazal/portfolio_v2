c828eaa5702e5ddd69ab6f542b5093abb5895152
williams <w.chazal@codeur.online>
2021-11-10 16:08:41 +0100

    changement backoffice ajout bootstrap page projet

--- admin/projet.php
+++ admin/projet.php
@@ -18,33 +18,43 @@ if($_SESSION['username']){
 <?php include('./header_admin.php')?>
 
 <div class="container-fluid">
-<?php include('./navbar_admin.php')?>  
+  <div class="row align-items-start">
+        <div class="col-2 position-sticky">
+        <?php include('./navbar_admin.php')?>
+        </div>
 
-<div class="col">
-
-    <div class="col">
-        <div class="container-fluid--projet ">
-            <div class="col--projet">
-            <?php
+         <div class="col projet">
+         <?php
     foreach ($result as $projet) {
 ?>
-            <div class="card" style="width: 18rem;">
-
-                 <img src="assets_admin/admin_logo/<?= $projet['projet_logo'] ?>" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title"><?=$projet['projet_titre']?></h5>
-                    <p class="card-text"><?=$projet['projet_contexte']?></p>
-                    <a href="projet_detail.php?id=<?=$projet['idprojet']?>" class="btn btn-primary"><?=$projet['projet_titre']?></a>
-                </div>
-                </div>
-                <?php
+            <div class="card mt-4 mb-4">  
+                <h5 class="card-header "><img src="./assets_admin/admin_logo/<?= $projet['projet_logo'] ?>" class="card-img-top  logo" alt="..."><?=$projet['projet_titre']?> </h5>
+                    <div class="card-body text-center">
+                        <img src="./assets_admin/admin_img/<?= $projet['projet_image'] ?>" class='projets_details--image' class="card-img-top" alt="..." class="card-img-top" alt="...">
+                        <p class="card-text list-group-item"><?=$projet['projet_contexte']?></p>
+                        <p class="card-text list-group-item"><?=$projet['projet_specs']?></p>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item"><?=$projet['projet_date_debut']?> &nbsp; <?=$projet['projet_date_fin']?></li>
+                            <li class="list-group-item"<?=$projet['projet_type']?>></li>
+                            <li class="list-group-item"><a href="<?=$projet['projet_link']?> " target="_blank" class="btn btn-primary">lien du site</a>
+                             <a href="<?=$result['projet_github']?>" target="_blank" class="btn btn-primary">Github</a>
+                            </li>
+                        </ul>
+                        <a href="#" class="btn btn-warning">Modifier le projet <?=$projet['projet_titre']?></a>
+                        <a href="#" class="btn btn-danger">Supprimer le projet <?=$projet['projet_titre']?></a>
+                    </div>
+            </div>
+            <?php
     }
 ?>
-            </div>
-           <!--  <div class="col">test</div> -->
+
+        </div>
+
+        <div class="col">
+            One of three columns
         </div>
     </div>
-
 </div>
+
 
 <?php include('./footer_admin.php')?>
