bc3c9f34bbef3c07ae14866995e3457557be55bc
WilliamsChazal <w.chazal@codeur.online>
2021-11-12 23:58:13 +0100

    ajout edit design

--- admin/home.php
+++ admin/home.php
@@ -9,19 +9,18 @@ session_start();
   <div class="row align-items-start">
         <div class="col-2 position-sticky">
         <?php include('./navbar_admin.php')?>
-        </div>
-
-         <div class="col ">
-            <div class="row">BONJOUR SUR VOTRE BACK OFFICE <strong><?php echo $_SESSION['username'];?></strong> </div>
-            <div class="row">BONJOUR SUR VOTRE BACK OFFICE <strong><?php echo $_SESSION['username'];?></strong> </div>
-            <div class="row">BONJOUR SUR VOTRE BACK OFFICE <strong><?php echo $_SESSION['username'];?></strong> </div>
          </div>
+          <div class="col-10">
+            <div class="container ">
+              <h1>Back-Office</h1>
+              <div class="row">
+                    hello
+              </div>
+            </div>
+          </div>
 
-        </div>
 
-        <div class="col">
-            One of three columns
-        </div>
+
     </div>
 </div>
    
