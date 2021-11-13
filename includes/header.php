f24ccc2973cbb2709cd8111a4fb3b313ec6f6157
WilliamsChazal <w.chazal@codeur.online>
2021-11-13 21:57:31 +0100

    mise en prod

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Portoflio personnel de williams Chazal, Welccome to my World">
         <link rel="shortcut icon" href="../assets/img/logo.png" type="image/x-icon">
         <link rel="stylesheet" href="../styles/main.css">
         <link rel="stylesheet" href="../styles/libs/ressources.css">
@@ -21,6 +22,8 @@
         <link rel="stylesheet" href="../styles/libs/icons.css">
         <script src="https://unpkg.com/uuid@latest/dist/umd/uuidv4.min.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="http://fonts.cdnfonts.com/css/digital-7-mono" rel="stylesheet">
+                
 
         <title>Williams Chazal</title>
     </head>
